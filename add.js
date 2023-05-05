7361e66b0486dfa21c7c8324d98765b586e1a57e
msangameshwar <msangameshwar96@gmail.com>
2023-05-06 01:43:40 +0530

    feat: Negatives removed and two number addition performed

--- add.js
+++ add.js
@@ -1,9 +1,9 @@
 function add(numbers) {
-  let delimiter = ',';
   //Check if number is empty
-  if (numbers == '') {
+  if (!numbers) {
     return 0;
   }
+  let delimiter = ',';
   // Check for the delimiter
   if (numbers.startsWith('//')) {
     delimiter = numbers[2];
@@ -11,7 +11,24 @@ function add(numbers) {
   }
 
   let arr = numbers.split('\n').join(delimiter).split(delimiter);
-  console.log(arr);
+  let sum = 0;
+  let negatives = [];
+
+  arr.forEach((value) => {
+    let num = parseInt(value);
+
+    if (num < 0) {
+      negatives.push(num);
+    }
+    sum += num;
+  });
+
+  if (negatives.length) {
+    throw new Error('Negatives not allowed: ' + negatives.join(','));
+  }
+  return sum;
 }
 
 console.log(add(''));
+console.log(add('1,2'));
+console.log(add('1'));
