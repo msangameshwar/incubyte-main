7d176686e618611c8ddeca8ddee22d2b623422f6
msangameshwar <msangameshwar96@gmail.com>
2023-05-06 01:18:26 +0530

    feat: added condition to check delimiter

--- add.js
+++ add.js
@@ -1,7 +1,17 @@
 function add(numbers) {
+  let delimiter = ',';
+  //Check if number is empty
   if (numbers == '') {
     return 0;
   }
+  // Check for the delimiter
+  if (numbers.startsWith('//')) {
+    delimiter = numbers[2];
+    numbers = numbers.substring(4);
+  }
+
+  let arr = numbers.split('\n').join(delimiter).split(delimiter);
+  console.log(arr);
 }
 
 console.log(add(''));
